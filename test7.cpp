643983e5d88c0a3e2e2e66ee51b0a1c7168869a8
Suraj Patra <patrasuraj2000@gmail.com>
2020-10-28 10:36:46 +0530

    initial commit

---
+++ test7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+    int i,j;
+    std::cout << "Enter number";
+    std::cin >> i;
+    std::cout << "Enter number";
+    std::cin >> j;
+    std::cout << i+j;
+    return 0;
+}
